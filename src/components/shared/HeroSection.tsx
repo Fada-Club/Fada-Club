469fb951d725d4f36243107819505d0c2bed2fcd
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-07-10 17:07:52 +0530

    first commit

---
+++ src/components/shared/HeroSection.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-row justify-center my-48">
+     <div className="flex flex-col items-center gap-2">
+     <div className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <span className="">Projects</span> that Spark
+    </div>
+        <div className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          More than Just Interest
+        </div>
+        <div className="scroll-m-20 text-xl font-semibold tracking-tight">
+          Develop projects, learn, and grow in a welcoming space!
+        </div>
+     </div>
+    </div>
+  )
+}
+
+export default HeroSection
